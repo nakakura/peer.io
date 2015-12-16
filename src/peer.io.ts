b689a10d7ad5a7cd17d80db2d7f2baa37cc0b4b1
Toshiya Nakakura <nakakura@me.com>
2015-12-16 13:27:25 +0900

    dataを受け取れるlistener追加

--- src/peer.io.ts
+++ src/peer.io.ts
@@ -10,13 +10,22 @@
 
 module PeerIo{
     import NeighbourIf = Model.NeighbourIf;
-    export class PeerIo extends EventEmitter2{
+
+    export enum EventTypeEnum{
+        video = 1,
+        data = 2
+    }
+
+    type DataListener = (peerId: string, message: string)=>void;
+    type MediaListener = (peerId: string, stream: MediaStream)=>void;
+
+    export class PeerIo{
         private _peerJsManager: Model.PeerJsManager;
         private _targetNeighbours: Model.TargetNeighbours;
+        private _dataListener: Array<DataListener> = [];
+        private _mediaListener: Array<MediaListener> = [];
 
         constructor(peerJs: PeerJs.Peer){
-            super();
-
             this._targetNeighbours = new Model.TargetNeighbours();
             this._peerJsManager = new Model.PeerJsManager(peerJs);
             this._peerJsManager.addNeighboursSource("targetNeighbours", this._targetNeighbours.targetNeighbours);
@@ -34,12 +43,57 @@ module PeerIo{
         }
 
         onLinkEstablish = (neighbour: NeighbourIf)=>{
-            if(neighbour.type() === Model.NeighbourTypeEnum.video){
-                neighbour.onStream(this.onStream);
+            switch(neighbour.type()){
+                case Model.NeighbourTypeEnum.video:
+                    neighbour.onStream(this._notifyMedia);
+                    break;
+                case Model.NeighbourTypeEnum.data:
+                    neighbour.onData(this._notifyData);
+                    break;
             }
         };
 
-        onStream = (peerId: string, stream: MediaStream)=>{};
+        on(event: EventTypeEnum, listener: MediaListener | DataListener) {
+            switch(event){
+                case EventTypeEnum.video:
+                    this._mediaListener.push(<MediaListener>listener);
+                    break;
+                case EventTypeEnum.data:
+                    this._dataListener.push(<DataListener>listener);
+                    break;
+            }
+        }
+
+        off(event: EventTypeEnum, listener: MediaListener | DataListener){
+            var array: Array<MediaListener | DataListener> = [];
+
+            switch(event){
+                case EventTypeEnum.video:
+                    array = this._mediaListener;
+                    break;
+                case EventTypeEnum.data:
+                    array = this._dataListener;
+                    break;
+            }
+
+            var index = _.findIndex(array, (item)=>{
+                return item == listener;
+            });
+
+            if(index !== -1) array.splice(index, 1);
+        }
+
+        private _notifyMedia(neighbourID: string, stream: MediaStream){
+            _.each(this._mediaListener, (listener)=>{
+                listener(neighbourID, stream);
+            });
+        }
+
+        private _notifyData(neighbourID: string, message: string){
+            _.each(this._dataListener, (listener)=>{
+                listener(neighbourID, message);
+            });
+        }
     }
 }
 
